7ddbe8ea040defff2d8b47e806e09c02233b321b
ngusu <musyimi@users.noreply.github.com>
2022-09-07 13:58:21 +0300

    working on notifications to be finished later

---
+++ notification/src/main/java/com/musyimiworks/notification/Notification.java
@@ -0,0 +1,19 @@
+package com.musyimiworks.notification;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import lombok.NoArgsConstructor;
+
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString
+
+public class Notification {
+
+}
